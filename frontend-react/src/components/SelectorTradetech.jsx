f843d7893e0ca0df8e36cf9245a9453839c90914
Juan Carlos Ordoñez <terceroordonez1972@gmail.com>
2025-07-20 23:48:06 -0500

    Fix: se agregó import React a SelectorTradetech

--- frontend-react/src/components/SelectorTradetech.jsx
+++ frontend-react/src/components/SelectorTradetech.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
